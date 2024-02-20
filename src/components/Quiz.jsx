fe7830ea2612a34e2c4f3b0fdd284e857ae0777c
zalesiu <zalewmat31@gmail.com>
2024-02-20 16:43:49 +0100

    feat: update template in Quiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+import questions from "../Helpers/QuestionsData";
+
 function Quiz() {
-  return <div>Quiz</div>;
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [optionChosen, setOptionChosen] = useState("");
+
+  const handleNextQuestion = () => {};
+
+  return (
+    <div className="Quiz">
+      <h1>{questions[currentQuestion].text}</h1>
+      <div className="Quiz-options">
+        <button onClick={() => setOptionChosen("A")}>
+          {questions[currentQuestion].optionA}
+        </button>
+        <button onClick={() => setOptionChosen("B")}>
+          {questions[currentQuestion].optionB}
+        </button>
+        <button onClick={() => setOptionChosen("C")}>
+          {questions[currentQuestion].optionC}
+        </button>
+        <button onClick={() => setOptionChosen("D")}>
+          {questions[currentQuestion].optionD}
+        </button>
+      </div>
+      <button onClick={handleNextQuestion}>Next question</button>
+    </div>
+  );
 }
 
 export default Quiz;
