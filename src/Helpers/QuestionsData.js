4582353ba0b7a362823c7c4277a774cd653906de
zalesiu <zalewmat31@gmail.com>
2024-02-20 16:27:55 +0100

    feat: create QuestionsData helper, add dummy content

---
+++ src/Helpers/QuestionsData.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    text: "What is the longest river in the world?",
+    optionA: "The Rhine",
+    optionB: "The Amazon River",
+    optionC: "The Mekong",
+    optionD: "The Nile",
+    answer: "optionD",
+  },
+  {
+    text: "What country has the most islands in the world?",
+    optionA: "Sweden",
+    optionB: "Canada",
+    optionC: "Finland",
+    optionD: "Philippines",
+    answer: "optionC",
+  },
+  {
+    text: "What’s the smallest country in the world?",
+    optionA: "The Vatican",
+    optionB: "Qatar",
+    optionC: "Iceland",
+    optionD: "Liechtenstein",
+    answer: "optionA",
+  },
+];
+
+export default questions;
